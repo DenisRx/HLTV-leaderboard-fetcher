b0683706367eac1ae4c1cc233304a32957b33546
DenisRx <denis.roux@epitech.eu>
2023-12-31 12:15:31 +0100

    Add default root endpoint

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const { HLTV } = require('hltv')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Welcome to HLTV-leaderboard-fetcher API.')
+})
 
 app.get('/leaderboard', async (req, res) => {
     try {
-        const teamRanking = await HLTV.getTeamRanking();
-        res.send(teamRanking);
+        const teamRanking = await HLTV.getTeamRanking()
+        res.send(teamRanking)
     } catch (e) {
         console.error(e)
     }
 })
 
 app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+    console.log(`App listening on port ${port}`)
 });
